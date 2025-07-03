10435083e3ced4f94a3a6d1fdde23b8605c40e3f
St3foo <stefoo89@gmail.com>
2025-07-03 12:57:29 +0300

    Add project files.

---
+++ TravelAgency.Data/Data/TravelAgencyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelAgency.Data
+{
+    public class TravelAgencyDbContext : IdentityDbContext
+    {
+        public TravelAgencyDbContext(DbContextOptions<TravelAgencyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
